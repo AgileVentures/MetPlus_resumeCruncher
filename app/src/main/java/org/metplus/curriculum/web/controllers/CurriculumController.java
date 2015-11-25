dbb8f5a8794c9f7b6c32a7d9d1b3ad0895251d59
Joao Pereira <joaopapereira@gmail.com>
2015-11-25 15:52:20 -0500

    Download succesfull

--- app/src/main/java/org/metplus/curriculum/web/controllers/CurriculumController.java
+++ app/src/main/java/org/metplus/curriculum/web/controllers/CurriculumController.java
@@ -1,21 +1,15 @@
 package org.metplus.curriculum.web.controllers;
 
 
-import com.mongodb.BasicDBObject;
-import com.mongodb.gridfs.GridFS;
-import com.mongodb.gridfs.GridFSInputFile;
 import org.metplus.curriculum.database.config.SpringMongoConfig;
 import org.metplus.curriculum.database.domain.Resume;
 import org.metplus.curriculum.database.exceptions.ResumeNotFound;
 import org.metplus.curriculum.database.exceptions.ResumeReadException;
 import org.metplus.curriculum.database.repository.ResumeRepository;
-import org.metplus.curriculum.exceptions.CurriculumException;
 import org.metplus.curriculum.web.GenericAnswer;
 import org.metplus.curriculum.web.ResultCodes;
 import  org.apache.log4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -81,38 +75,31 @@ public class CurriculumController {
         return new ResponseEntity<>(answer, HttpStatus.OK);
     }
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @RequestMapping(value = "/{userId}", method = RequestMethod.GET)
     @ResponseBody
     public ResponseEntity<?> getCurriculum(
-            @RequestParam("userId") String id,
+            @PathVariable("userId") String id,
             HttpServletResponse response) {
         LOG.debug("Retrieving curriculum of user: '" + id + "'");
         GenericAnswer answer = new GenericAnswer();
 
         Resume resume = resumeRepository.findByUserId(id);
         if(resume == null) {
+            LOG.warn("Unable to find user: '" + id + "'");
             answer.setMessage("Unable to find the user: '" + id + "'");
             answer.setResultCode(ResultCodes.RESUME_NOT_FOUND);
             return new ResponseEntity<GenericAnswer>(answer, HttpStatus.OK);
         }
-        ByteArrayOutputStream file = null;
         try {
-            file = resume.getResume(dbConfig);
-
-            HttpHeaders respHeaders = new HttpHeaders();
-            //respHeaders.setContentType(new MediaType("application", resume.getFileType()));
-            respHeaders.setContentType(new MediaType("application", "file"));
-            respHeaders.setContentLength(file.size());
-            respHeaders.setContentDispositionFormData("attachment", resume.getFilename());
+            ByteArrayOutputStream file = resume.getResume(dbConfig);
+            response.setContentType("application/octet-stream");
+            response.setHeader("Content-Disposition", String.format("inline; filename=\"" + resume.getFilename() +"\""));
             FileCopyUtils.copy(file.toByteArray(), response.getOutputStream());
             response.setContentLength(file.size());
-            response.setContentType(MediaType.APPLICATION_OCTET_STREAM_VALUE);
-            response.setHeader("Content-Disposition", "inline; filename=\"" + resume.getFilename() + "\"");
             response.flushBuffer();
-            LOG.debug(response.getHeader("Content-Disposition"));
-
             return null;
         } catch (ResumeNotFound resumeNotFound) {
+            LOG.warn("Unable to find curriculum for user: '" + id + "'");
             answer.setMessage("Unable to find the resume for the user");
             answer.setResultCode(ResultCodes.RESUME_NOT_FOUND);
         } catch (ResumeReadException e) {
@@ -121,10 +108,11 @@ public class CurriculumController {
             answer.setMessage("Unable to retrieve resume");
             answer.setResultCode(ResultCodes.FATAL_ERROR);
         } catch (IOException e) {
-            e.printStackTrace();
+            LOG.error("Error reading resume for user: '" + id + "'");
+            LOG.error(e.getStackTrace());
+            answer.setMessage("Unable to read resume");
+            answer.setResultCode(ResultCodes.FATAL_ERROR);
         }
-
-        LOG.debug("Result:" + answer);
 
         return new ResponseEntity<GenericAnswer>(answer, HttpStatus.OK);
     }
