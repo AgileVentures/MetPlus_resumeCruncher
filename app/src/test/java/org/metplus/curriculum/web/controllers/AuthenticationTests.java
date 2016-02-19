c024cd5bae3600b6e5b99fd62e84d76b167c7cf7
Joao Pereira <joaopapereira@gmail.com>
2016-02-18 19:27:26 -0500

    Added first steps of tests

---
+++ app/src/test/java/org/metplus/curriculum/web/controllers/AuthenticationTests.java
@@ -0,0 +1,38 @@
+package org.metplus.curriculum.web.controllers;
+
+/**
+ * Created by joao on 2/18/16.
+ */
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class AuthenticationTests {
+
+    public static final Logger logger = LoggerFactory.getLogger(AdminControllerTest.class);
+    @Autowired
+    private WebApplicationContext ctx;
+    @Before
+    public void setUp() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(ctx).build();
+    }
+    private MockMvc mockMvc;
+
+    @Test
+    public void test() throws Exception {
+
+        MockHttpServletResponse response = mockMvc.perform(get("/api/v1/authenticate")
+                                    .accept(MediaType.APPLICATION_JSON))
+                                    .andExpect(status().isOk()).andReturn().getResponse();
+    }
+}
