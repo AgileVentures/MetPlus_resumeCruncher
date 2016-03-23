b6c5f52b60bf65a3ab7ea15cee4e77e5c330bcc9
Joao Pereira <joaopapereira@gmail.com>
2016-03-23 17:44:03 -0400

    Add Job Model information

--- database/src/main/java/org/metplus/curriculum/database/domain/Job.java
+++ database/src/main/java/org/metplus/curriculum/database/domain/Job.java
@@ -3,19 +3,63 @@ package org.metplus.curriculum.database.domain;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
-import java.util.Map;
-
 /**
- * Created by joao on 3/23/16.
+ * Job Model
  */
 @Document
-public class Job extends AbstractDocument {
+public class Job extends DocumentWithMetaData {
     @Field
     private String title;
     @Field
     private String jobId;
     @Field
     private String description;
-    @Field
-    private Map<String, MetaData> metaData;
+
+    /**
+     * Retrieve the title of the Job
+     * @return Job Title
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * Set the Job Title
+     * @param title Job Title
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    /**
+     * Retrieve the job identifier
+     * @return Job Identifier
+     */
+    public String getJobId() {
+        return jobId;
+    }
+
+    /**
+     * Set the job identifier
+     * @param jobId Job identifier
+     */
+    public void setJobId(String jobId) {
+        this.jobId = jobId;
+    }
+
+    /**
+     * Retrieve the job description
+     * @return Job Description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * Set the job description
+     * @param description Job Description
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
