6ffec25e1a9c52bb047309cadb011cfbf3243530
Joao Pereira <joaopapereira@gmail.com>
2016-05-28 17:55:58 -0400

    Added default values and loading information

--- naiveBayesClassifier/src/main/java/org/metplus/curriculum/cruncher/naivebayes/CruncherImpl.java
+++ naiveBayesClassifier/src/main/java/org/metplus/curriculum/cruncher/naivebayes/CruncherImpl.java
@@ -1,19 +1,43 @@
 package org.metplus.curriculum.cruncher.naivebayes;
 
-
+import de.daslaboratorium.machinelearning.classifier.bayes.BayesClassifier;
 import org.metplus.curriculum.cruncher.Cruncher;
 import org.metplus.curriculum.cruncher.CruncherMetaData;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by joao on 5/28/16.
  */
 public class CruncherImpl implements Cruncher {
     public static final String CRUNCHER_NAME = "NaiveBayes";
+
+    private BayesClassifier<String, String> classifier;
+
+    public CruncherImpl() {
+        classifier = new BayesClassifier();
+    }
+
     public CruncherMetaData crunch(String data) {
         return null;
     }
 
     public String getCruncherName() {
         return CRUNCHER_NAME;
+    }
+
+    public void resetMemory() {
+        classifier.reset();
+    }
+
+    public void train(Map<String, List<String>> database) {
+        for(Map.Entry<String, List<String>> entry: database.entrySet()) {
+            train(entry.getKey(), entry.getValue());
+        }
+    }
+
+    public void train(String feature, List<String> trainningSet) {
+        classifier.learn(feature, trainningSet);
     }
 }
--- naiveBayesClassifier/src/main/java/org/metplus/curriculum/cruncher/naivebayes/NaiveBayesCruncher.java
+++ naiveBayesClassifier/src/main/java/org/metplus/curriculum/cruncher/naivebayes/NaiveBayesCruncher.java
@@ -2,21 +2,110 @@ package org.metplus.curriculum.cruncher.naivebayes;
 
 import org.metplus.curriculum.cruncher.Cruncher;
 import org.metplus.curriculum.cruncher.Matcher;
+import org.metplus.curriculum.database.domain.CruncherSettings;
+import org.metplus.curriculum.database.domain.Setting;
+import org.metplus.curriculum.database.domain.Settings;
+import org.metplus.curriculum.database.exceptions.CruncherSettingsNotFound;
+import org.metplus.curriculum.database.repository.SettingsRepository;
 import org.metplus.curriculum.init.CruncherInitializer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.Map;
+import java.util.NoSuchElementException;
+
 /**
- * Created by joao on 5/28/16.
+ * Class used for initialization of the cruncher based on a
+ * Naive Bayes Classifier
  */
 @Component
-@ConfigurationProperties(locations = "classpath:expressionCruncher.yml", prefix = "config")
+@ConfigurationProperties(locations = "classpath:naiveBayes.yml", prefix = "config")
 public class NaiveBayesCruncher extends CruncherInitializer {
 
+    private static final String LEARN_DATABASE = "LearnDatabase";
+    private static final String CLEAN_EXPRESSIONS = "CleanExpressions";
+
     private CruncherImpl cruncherImpl;
+    private Map<String, List<String>> learnDatabase;
+    private List<String> cleanExpressions;
+
+    public Map<String, List<String>> getLearnDatabase() {
+        return learnDatabase;
+    }
+
+    public void setLearnDatabase(Map<String, List<String>> learnDatabase) {
+        this.learnDatabase = learnDatabase;
+    }
+
+
+    public List<String> getCleanExpressions() {
+        return cleanExpressions;
+    }
+
+    public void setCleanExpressions(List<String> cleanExpressions) {
+        this.cleanExpressions = cleanExpressions;
+    }
+
+
+    @Autowired
+    private SettingsRepository repository;
+
+    public NaiveBayesCruncher() {
+
+    }
+
+    public NaiveBayesCruncher(SettingsRepository repository) {
+        this.repository = repository;
+    }
+
     @Override
     public void init() {
+        try {
+            cruncherImpl = new CruncherImpl();
+            load();
+        } catch (CruncherSettingsNotFound cruncherSettingsNotFound) {
+            save();
+        }
+    }
 
+    public void reload() {
+        try {
+            load();
+        } catch (CruncherSettingsNotFound cruncherSettingsNotFound) {
+        }
+    }
+
+    private void load() throws CruncherSettingsNotFound{
+        cruncherImpl.resetMemory();
+        try {
+            CruncherSettings settings;
+            try {
+                settings = repository.findAll().iterator().next().getCruncherSettings(CruncherImpl.CRUNCHER_NAME);
+            } catch(NoSuchElementException e) {
+                repository.save(new Settings());
+                settings = repository.findAll().iterator().next().getCruncherSettings(CruncherImpl.CRUNCHER_NAME);
+            }
+
+            learnDatabase = (Map)settings.getSetting(LEARN_DATABASE).getData();
+            cruncherImpl.train(learnDatabase);
+        } finally {
+            learnDatabase = null;
+        }
+    }
+
+    public void save() {
+        CruncherSettings cSettings;
+        try {
+            cSettings = repository.findAll().iterator().next().getCruncherSettings(CruncherImpl.CRUNCHER_NAME);
+        } catch (CruncherSettingsNotFound cruncherSettingsNotFound) {
+            cSettings = new CruncherSettings(CruncherImpl.CRUNCHER_NAME);
+        }
+        cSettings.addSetting(new Setting<>(LEARN_DATABASE, learnDatabase));
+        Settings globalSettings = repository.findAll().iterator().next();
+        globalSettings.addCruncherSettings(CruncherImpl.CRUNCHER_NAME, cSettings);
+        repository.save(globalSettings);
     }
 
     @Override
