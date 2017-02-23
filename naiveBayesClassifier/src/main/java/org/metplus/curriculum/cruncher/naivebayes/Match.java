c243167ad16a5d0851e9c0eeb11a14fef0983c3d
Joao Pereira <joaopapereira@gmail.com>
2017-02-22 19:09:30 -0500

    Removed temporary interface

--- naiveBayesClassifier/src/main/java/org/metplus/curriculum/cruncher/naivebayes/Match.java
+++
@@ -1,8 +0,0 @@
-package org.metplus.curriculum.cruncher.naivebayes;
-
-import java.util.List;
-
-public interface Match<Entry, Result> {
-    List<Result> match(Entry entry);
-    List<Entry> matchInverse(Result entry);
-}
