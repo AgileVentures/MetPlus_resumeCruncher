c3abd14a429cc4144fd3a0f13105025c9a2775a0
Joao Pereira <joaopapereira@gmail.com>
2018-07-27 17:13:16 -0400

    Clean slate

--- core/src/main/java/org/metplus/curriculum/cruncher/CruncherMetaData.java
+++
@@ -1,9 +0,0 @@
-package org.metplus.curriculum.cruncher;
-
-/**
- * Created by joao on 3/16/16.
- * Basic interface used only to signal that the object
- * represent the meta data that a cruncher generates
- */
-public interface CruncherMetaData {
-}
--- database/src/main/java/org/metplus/curriculum/database/template/TemplatePackage.java
+++
@@ -1,7 +0,0 @@
-package org.metplus.curriculum.database.template;
-
-/**
- * Created by Joao Pereira on 26/08/2015.
- */
-public interface TemplatePackage {
-}
