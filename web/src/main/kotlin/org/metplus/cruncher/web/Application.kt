335155f03a6428c6b6266a067fe4823b88f74f94
Joao Pereira <jdealmeidapereira@pivotal.io>
2018-08-03 18:22:18 -0400

    Initial stuff

---
+++ web/src/main/kotlin/org/metplus/cruncher/web/Application.kt
@@ -0,0 +1,11 @@
+package org.metplus.cruncher.web
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+open class Application
+
+fun main(args: Array<String>) {
+    runApplication<Application>(*args)
+}
