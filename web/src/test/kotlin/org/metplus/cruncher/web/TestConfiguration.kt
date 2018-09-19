2c98e43f7b67151546d6546cd81ff031c9350faf
Joao Pereira <joaopapereira@gmail.com>
2018-09-19 17:01:55 -0400

    API endpoint to retrieve the Settings
    This commit will include the ability to retrieve the settings when no data is set on the database

--- web/src/test/kotlin/org/metplus/cruncher/web/TestConfiguration.kt
+++ web/src/test/kotlin/org/metplus/cruncher/web/TestConfiguration.kt
@@ -1,5 +1,6 @@
 package org.metplus.cruncher.web
 
+import org.metplus.cruncher.settings.GetSettings
 import org.metplus.cruncher.settings.SettingsRepository
 import org.metplus.cruncher.settings.SettingsRepositoryFake
 import org.springframework.boot.SpringBootConfiguration
@@ -11,4 +12,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc
 open class TestConfiguration {
     @Bean
     open fun getSettingsRepository(): SettingsRepository =  SettingsRepositoryFake()
+
+    @Bean
+    open fun getSettings(): GetSettings = GetSettings(getSettingsRepository())
 }
