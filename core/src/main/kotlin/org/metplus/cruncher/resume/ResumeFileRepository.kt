9b8938774bcf9665d51a3999a605a47850fe6315
Joao Pereira <joaopapereira@gmail.com>
2018-11-09 13:14:02 -0500

    Allow download of resumes
    Add the endpoint and the use case to download a resume

--- core/src/main/kotlin/org/metplus/cruncher/resume/ResumeFileRepository.kt
+++ core/src/main/kotlin/org/metplus/cruncher/resume/ResumeFileRepository.kt
@@ -2,6 +2,6 @@ package org.metplus.cruncher.resume
 
 interface ResumeFileRepository {
     fun save(resumeFile: ResumeFile): ResumeFile
-    fun getByUserId(userId: String): ResumeFile?
+    fun getByUserId(userId: String): ResumeFile
     fun deleteIfExists(userId: String)
 }
