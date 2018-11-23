b71c0f2fd4de15322028845dad94fdb9571e7dcf
Joao Pereira <joaopapereira@gmail.com>
2018-11-23 11:10:03 -0500

    WIP: Started working in adding naivebayes classifier to the app

---
+++ core/src/main/kotlin/org/metplus/cruncher/utilities/DocumentParser.kt
@@ -0,0 +1,53 @@
+package org.metplus.cruncher.utilities
+
+import org.apache.tika.exception.TikaException
+import org.apache.tika.parser.AutoDetectParser
+import org.apache.tika.sax.BodyContentHandler
+import org.apache.tika.metadata.Metadata
+import org.metplus.cruncher.error.DocumentParseException
+import org.slf4j.LoggerFactory
+import org.xml.sax.SAXException
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
+import java.io.IOException
+
+class DocumentParserImpl(stream: ByteArrayOutputStream) {
+
+    private var stream: ByteArrayInputStream = ByteArrayInputStream(stream.toByteArray())
+    private var document: String? = null
+
+    @Throws(DocumentParseException::class)
+    fun parse() {
+        val parser = AutoDetectParser()
+        val handler = BodyContentHandler()
+        val metadata = Metadata()
+        try {
+            parser.parse(stream, handler, metadata)
+            document = handler.toString()
+        } catch (e: SAXException) {
+            throw DocumentParseException(e.message)
+        } catch (e: TikaException) {
+            throw DocumentParseException(e.message)
+        } catch (e: IOException) {
+            throw DocumentParseException(e.message)
+        } finally {
+            try {
+                stream.close()
+            } catch (e: IOException) {
+                LOG.warn("Unable to close the stream: " + e.message)
+            }
+
+        }
+    }
+
+    @Throws(DocumentParseException::class)
+    fun getDocument(): String? {
+        if (document == null)
+            parse()
+        return document
+    }
+
+    companion object {
+        private val LOG = LoggerFactory.getLogger(DocumentParserImpl::class.java)
+    }
+}
--- web/src/test/kotlin/org/metplus/cruncher/web/controller/ResumeControllerTest.kt
+++ web/src/test/kotlin/org/metplus/cruncher/web/controller/ResumeControllerTest.kt
@@ -102,7 +102,7 @@ internal class ResumeControllerTest(@Autowired private val mvc: MockMvc) {
                 userId = "asdasdasd",
                 filename = "line_with_bold.pdf",
                 fileType = "pdf",
-                cruncherData = CruncherMetaData(mutableMapOf<String, Long>() as HashMap<String, Long>)
+                cruncherData = CruncherMetaData(mutableMapOf<String, Double>() as HashMap<String, Double>)
         ))
         resumeFileRepository.save(ResumeFile(
                 userId = "asdasdasd",
