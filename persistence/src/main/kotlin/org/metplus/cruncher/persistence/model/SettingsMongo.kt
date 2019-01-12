9edad759320d477f3a9c1ce15910ae9f485bed37
Joao Pereira <joaopapereira@gmail.com>
2019-01-11 19:01:00 -0500

    Correct the name of the mongo collections expected by the models

--- persistence/src/main/kotlin/org/metplus/cruncher/persistence/model/SettingsMongo.kt
+++ persistence/src/main/kotlin/org/metplus/cruncher/persistence/model/SettingsMongo.kt
@@ -5,8 +5,7 @@ import org.springframework.data.annotation.TypeAlias
 import org.springframework.data.mongodb.core.mapping.Document
 import java.math.BigInteger
 
-@Document
-@TypeAlias("settings")
+@Document(collection = "settings")
 class SettingsMongo(
         @Id val id: BigInteger,
         val appSettingsMongo: SettingsListMongo,
