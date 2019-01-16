359400597370e9713d572b67e539b5b6ab5c9f62
Joao Pereira <joaopapereira@gmail.com>
2019-01-16 18:49:57 -0500

    Add file used to build and push docker images in CI

---
+++ build_docker_image.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+image_name=agileventures/metplus-cruncher:$BRANCH_NAME-$SEMAPHORE_BUILD_NUMBER
+image_branch_tag=agileventures/metplus-cruncher:$BRANCH_NAME
+
+docker build -f Dockerfile.build --tag ${image_name} .
+
+if [ "$BRANCH_NAME" = development ] || [ "$BRANCH_NAME" = master ]; then
+  docker tag ${image_name} ${image_branch_tag}
+  docker push ${image_name}
+  docker push ${image_branch_tag}
+fi
